d104f7da467ca538e84faf0f5873d3e93dde725a
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2021-03-06 15:57:12 -0500

    Init Tailwindcss CDN, font is added and ready to go. Also, `react-query` is installed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="App">
-      
-      
-    </div>
-  );
+	return (
+		<>
+			<div className="App bg-gray-50">Esmaeil MIRZAEE</div>
+		</>
+	);
 }
