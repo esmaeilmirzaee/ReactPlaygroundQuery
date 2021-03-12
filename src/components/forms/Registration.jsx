fe14d2bce55063ce8a3bc227965cbc9cd087dc6e
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2021-03-12 04:11:10 -0500

    Tailwindcss forms add the required package

--- src/components/forms/Registration.jsx
+++ src/components/forms/Registration.jsx
@@ -21,7 +21,7 @@ const Registration = () => {
 							value={values.email}
 							onChange={handleChange}
 						/>
-						<span className="error">{errors.email && errors.email}</span>
+						<span className="text-red-500">{errors.email && errors.email}</span>
 					</div>
 					<div className="form_inputs">
 						<label htmlFor="password" className="form_label">
@@ -34,24 +34,28 @@ const Registration = () => {
 							value={values.password}
 							onChange={handleChange}
 						/>
-						<span className="error">{errors.password && errors.password}</span>
+						<span className="text-red-500">
+							{errors.password && errors.password}
+						</span>
 					</div>
-					<div className="form_inputs">
+					<div className="flex flex-col justify-center mx-1">
 						<label htmlFor="password2" className="form_label">
 							Confirm password
 						</label>
 						<input
 							name="password2"
 							type="password"
-							className="form_input"
+							className="border rounded-lg py-2 px-3 border-gray-300 focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 autocomplete"
 							value={values.password2}
 							onChange={handleChange}
 						/>
-						<span className="error">
+						<span className="text-red-500">
 							{errors.password2 && errors.password2}
 						</span>
 					</div>
-					<button className="form_btn">Sign up</button>
+					<button className="bg-blue-600 px-5 py-3 rounded text-white m-5 w-full">
+						Sign up
+					</button>
 					<div className="form_info">
 						To login press <span className="form_btn_secondary">Login</span>
 					</div>
